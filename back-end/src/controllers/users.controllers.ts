cb6147e75a5a4dd88acbea0147813752e5377c59
phongnqse172516@gmail.com <phongnqse172516@gmail.com>
2024-05-21 12:36:24 +0700

    login - forgotpassword

--- back-end/src/controllers/users.controllers.ts
+++ back-end/src/controllers/users.controllers.ts
@@ -2,14 +2,33 @@ import { NextFunction, Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { body } from 'express-validator'
 import sendMail from '~/helper/send.mail'
-import { RegisterReqBody } from '~/model/requests/User.requests'
+import {
+  ForgotPasswordReqBody,
+  RegisterReqBody,
+  ResetPasswordReqBody,
+  TokenPayload,
+  VerifyForgotPasswordReqBody
+} from '~/model/requests/User.requests'
 import User from '~/model/schemas/User.schema'
 import databaseService from '~/services/database.services'
 import usersService from '~/services/users.services'
 import { generateEmailTemplate } from '~/helper/emailTemplate'
 import { USERS_MESSAGES } from '~/constants/messages'
 import { ObjectId } from 'mongodb'
+import HTTP_STATUS from '~/constants/httpStatus'
+import { UserVerifyStatus } from '~/constants/enums'
+
 //login:
+export const loginController = async (req: Request, res: Response) => {
+  const user = req.user as User // lấy user từ req
+  const user_id = user._id as ObjectId // lấy _id từ user
+  const result = await usersService.login(user_id.toString())
+
+  return res.status(400).json({
+    message: USERS_MESSAGES.LOGIN_SUCCESS,
+    result: result
+  })
+}
 
 //register:
 export const registerController = async (
@@ -17,17 +36,129 @@ export const registerController = async (
   res: Response,
   next: NextFunction
 ) => {
-  const verificationLink = 'https://www.facebook.com/huy.lequang.91712'
-  const emailHtml = generateEmailTemplate(req.body.username, verificationLink)
-  await sendMail({
-    email: req.body.email,
-    subject: 'Email Verification Mail',
-    html: emailHtml
-  })
   const result = await usersService.register(req.body) // thay luôn
+  // const verificationLink = `${process.env.BACKEND_URL}/verify-email`
+  // const emailHtml = generateEmailTemplate(req.body.username, verificationLink, result.email_verify_token)
+  // await sendMail({
+  //   email: req.body.email,
+  //   subject: 'Email Verification Mail',
+  //   html: emailHtml
+  // })
   console.log(result)
+  //console.log(verificationLink)
   return res.status(400).json({
     message: USERS_MESSAGES.REGISTER_SUCCESS,
     result: result
   })
+}
+
+//logout:
+export const logoutController = async (req: Request, res: Response) => {
+  const { refresh_token } = req.body
+  const result = await usersService.logout(refresh_token) //hàm trả ra chuỗi báo logout thành công
+  return res.json(result)
+}
+
+//verify:
+export const emailVerifyController = async (req: Request, res: Response, next: NextFunction) => {
+  // const { email_verify_token } = req.body
+  // const user = await databaseService.users.findOne({ email_verify_token: email_verify_token })
+  //ta có thể tìm user thông qua email_verify_token do người dùng gui lên lên thế này nhưng hiệu năng sẽ kém
+  //nên thay vào đó ta sẽ lấy thông tin _id của user từ decoded_email_verify_token mà ta thu đc từ middleware trước
+  //và tìm user thông qua _id đó
+  const { user_id } = req.decoded_email_verify_token as TokenPayload
+  const user = await databaseService.users.findOne({
+    _id: new ObjectId(user_id)
+  }) //hiệu năng cao hơn
+  //nếu k có user thì cho lỗi 404: not found
+
+  if (user === null) {
+    return res.status(HTTP_STATUS.NOT_FOUND).json({
+      message: USERS_MESSAGES.USER_NOT_FOUND
+    })
+  }
+  //nếu mà email_verify_token là rỗng: tức là account đã đc verify email trước đó rồi
+  //thì mình sẽ trả về status 200 ok, với message là đã verify email trước đó rồi
+  //chứ không trả về lỗi, nếu k thì client sẽ bối rối
+  if (user.email_verify_token === '') {
+    //mặc định là status 200
+    return res.json({
+      message: USERS_MESSAGES.EMAIL_ALREADY_VERIFIED_BEFORE
+    })
+  }
+  const result = await usersService.verifyEmail(user_id)
+  //để cập nhật lại email_verify_token thành rỗng và tạo ra access_token và refresh_token mới
+  //gữi cho người vừa request email verify đang nhập
+  return res.json({
+    message: USERS_MESSAGES.EMAIL_VERIFY_SUCCESS,
+    result: result
+  })
+}
+
+export const resendEmailVerifyController = async (req: Request, res: Response, next: NextFunction) => {
+  //khi đến đây thì accesstokenValidator đã chạy rồi => access_token đã đc decode
+  //và lưu vào req.user, nên trong đó sẽ có user._id để tao sử dụng
+  const { user_id } = req.decoded_authorization as TokenPayload //lấy user_id từ decoded_authorization
+  //từ user_id này ta sẽ tìm user trong database
+  const user = await databaseService.users.findOne({
+    _id: new ObjectId(user_id)
+  })
+  //nếu k có user thì trả về lỗi 404: not found
+  if (!user) {
+    return res.status(HTTP_STATUS.NOT_FOUND).json({
+      message: USERS_MESSAGES.USER_NOT_FOUND
+    })
+  }
+  //nếu user đã verify email trước đó rồi thì trả về lỗi 400: bad request
+  if (user.verify == UserVerifyStatus.Verified) {
+    return res.json({
+      message: USERS_MESSAGES.EMAIL_ALREADY_VERIFIED_BEFORE
+    })
+  }
+  //nếu user chưa verify email thì ta sẽ gữi lại email verify cho họ
+  //cập nhật email_verify_token mới và gữi lại email verify cho họ
+  const result = await usersService.resendEmailVerify(user_id)
+  //result chứa message nên ta chỉ cần trả  result về cho client
+  return res.json(result)
+}
+
+export const forgotPasswordController = async (
+  req: Request<ParamsDictionary, any, ForgotPasswordReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  //middleware forgotPasswordValidator đã chạy rồi, nên ta có thể lấy _id từ user đã tìm đc bằng email
+  const { _id } = req.user as User
+  //cái _id này là objectid, nên ta phải chuyển nó về string
+  //chứ không truyền trực tiếp vào hàm forgotPassword
+  const result = await usersService.forgotPassword((_id as ObjectId).toString())
+  return res.json(result)
+}
+
+export const verifyForgotPasswordTokenController = async (
+  req: Request<ParamsDictionary, any, VerifyForgotPasswordReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  //nếu đã đến bước này nghĩa là ta đã tìm có forgot_password_token hợp lệ
+  //và đã lưu vào req.decoded_forgot_password_token
+  //thông tin của user
+  //ta chỉ cần thông báo rằng token hợp lệ
+  return res.json({
+    message: USERS_MESSAGES.VERIFY_FORGOT_PASSWORD_TOKEN_SUCCESS
+  })
+}
+
+export const resetPasswordController = async (
+  req: Request<ParamsDictionary, any, ResetPasswordReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  //middleware resetPasswordValidator đã chạy rồi, nên ta có thể lấy đc user_id từ decoded_forgot_password_token
+  const { user_id } = req.decoded_forgot_password_token as TokenPayload
+  const { password } = req.body
+  //vào database tìm user thông qua user_id này và cập nhật lại password mới
+  //vì vào database nên ta sẽ code ở user.services
+  const result = await usersService.resetPassword(user_id, password) //ta chưa code resetPassword
+  return res.json(result)
 }
--- back-end/src/routes/users.routes.ts
+++ back-end/src/routes/users.routes.ts
@@ -1,10 +1,61 @@
 import { Router } from 'express'
-import { registerController } from '~/controllers/users.controllers'
-import { registerValidator } from '~/middlewares/users.middlewares'
+import {
+  accessTokenValidator,
+  emailVerifyTokenValidator,
+  forgotPasswordValidator,
+  loginValidator,
+  refreshTokenValidator,
+  registerValidator,
+  resetPasswordValidator,
+  verifyForgotPasswordTokenValidator
+} from '~/middlewares/users.middlewares'
+import {
+  emailVerifyController,
+  forgotPasswordController,
+  loginController,
+  logoutController,
+  registerController,
+  resendEmailVerifyController,
+  resetPasswordController,
+  verifyForgotPasswordTokenController
+} from '~/controllers/users.controllers'
 import { wrapAsync } from '~/utils/handlers'
 const usersRouter = Router()
-// usersRouter.post('/login', loginValidator, loginController)
-
+//user:
 usersRouter.post('/register', registerValidator, wrapAsync(registerController))
+usersRouter.post('/login', loginValidator, wrapAsync(loginController))
+usersRouter.post('/logout', accessTokenValidator, refreshTokenValidator, wrapAsync(logoutController))
+usersRouter.post('/verify-email', emailVerifyTokenValidator, wrapAsync(emailVerifyController))
+usersRouter.post('/resend-verify-email', accessTokenValidator, wrapAsync(resendEmailVerifyController))
+/*
+des: cung cấp email để reset password, gữi email cho người dùng
+path: /forgot-password
+method: POST
+Header: không cần, vì  ngta quên mật khẩu rồi, thì sao mà đăng nhập để có authen đc
+body: {email: string}
+*/
+usersRouter.post('/forgot-password', forgotPasswordValidator, wrapAsync(forgotPasswordController))
 //registerValidator là middleware để validate dữ liệu được dùng checkSchema của express-validator
+
+/*
+des: Verify link in email to reset password
+path: /verify-forgot-password
+method: POST
+Header: không cần, vì  ngta quên mật khẩu rồi, thì sao mà đăng nhập để có authen đc
+body: {forgot_password_token: string}
+*/
+usersRouter.post(
+  '/verify-forgot-password',
+  verifyForgotPasswordTokenValidator,
+  wrapAsync(verifyForgotPasswordTokenController)
+)
+
+/*
+des: reset password
+path: '/reset-password'
+method: POST
+Header: không cần, vì  ngta quên mật khẩu rồi, thì sao mà đăng nhập để có authen đc
+body: {forgot_password_token: string, password: string, confirm_password: string}
+*/
+usersRouter.post('/reset-password', resetPasswordValidator, wrapAsync(resetPasswordController))
 export default usersRouter
