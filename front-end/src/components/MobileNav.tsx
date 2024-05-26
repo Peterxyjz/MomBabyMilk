6fdb174b5ad6f33ad5bebcd3824bdf176f64c61d
Peterxyjz <huyq09@gmail.com>
2024-05-26 14:25:46 +0700

    update frontend

--- front-end/src/components/MobileNav.tsx
+++
@@ -1,9 +0,0 @@
-
-
-const MobileNav = () =>{
-    return(
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit impedit laudantium veritatis fuga dolorum vel necessitatibus, porro dolore. Rem fugit id dolore esse adipisci eveniet suscipit quam obcaecati expedita non!</div>
-    )
-}
-
-export default MobileNav;
